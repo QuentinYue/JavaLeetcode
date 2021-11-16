413b0187fa3011e53e4b4628e618ccad43e8d63a
rockyyau <yuyaokun2818@126.com>
2021-11-16 00:08:37 -0700

    201. Bitwise AND of Numbers Range Solution using left shifts and right shifts Method 2 (using Integer.toBinaryString())

---
+++ src/bitwiseRangeM2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+
+        if(Integer.toBinaryString(left).length() != Integer.toBinaryString(right).length()){
+            return 0;
+        }
+        int count = 0;
+        while(left != right){
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+        return left << count;
+    }
+}
