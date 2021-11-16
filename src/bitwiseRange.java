5425f1cb07fb6d9591a29d588c9ad83a27d18480
rockyyau <yaokun2@ualberta.ca>
2021-11-15 23:20:21 -0700

    201. Bitwise AND of Numbers Range Solution using left shifts and right shifts

---
+++ src/bitwiseRange.java
@@ -0,0 +1,26 @@
+public class bitwiseRange {
+    public static void main(String[] args){
+
+    }
+}
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        if(left == 0 || right == 0){
+            return 0;
+        }
+        int count = 0;
+        int res = 0;
+        while(left>=1 && right>=1){
+            if(left == right){
+                left = left << count;//finally does left shift 'count' bit
+                res = left;
+                break;
+            }
+            left = left >> 1; //keep doing right shift til left == right
+            right = right >> 1;
+            count++;
+        }
+        return res;
+    }
+}
