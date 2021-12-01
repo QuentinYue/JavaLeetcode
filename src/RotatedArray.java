7b617acebe0fca1d951cedccd8cb1925abc6d7f6
rockyyau <yuyaokun2818@126.com>
2021-12-01 01:04:42 -0700

    33. Search in Rotated Sorted Array Medium binary search method better to understand

--- src/RotatedArray.java
+++ src/RotatedArray.java
@@ -37,3 +37,37 @@ public class RotatedArray {
         System.out.println(nums[l] == target?l:-1);
     }
 }
+
+/*
+石 2, easier to understand:
+class Solution {
+    public int search(int[] nums, int t) {
+        //二分
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            int m = l + (r-l)/2;
+            if(nums[m] == t){
+                return m;
+            }
+            if(nums[m] >= nums[l]){ //必须加等于号
+                if(nums[l] <= t && t < nums[m]){
+                    r = m - 1;
+                }
+                else{
+                    l = m + 1;
+                }
+            }
+            else{
+                if(nums[m] < t && t <= nums[r]){
+                    l = m + 1;
+                }
+                else{
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+ */
